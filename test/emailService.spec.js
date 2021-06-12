0df37f3f07bc85200236a5642b237115b32b6051
1onlinesolution <team@damianos.io>
2021-06-12 16:54:54 +0100

    Fixed node.js.yml

--- test/emailService.spec.js
+++ test/emailService.spec.js
@@ -3,13 +3,6 @@ const EmailService = require('../lib/emailService');
 const emailConfig = require('../lib/defaultEmailConfig')({});
 
 describe('EmailHandler', () => {
-  const message = {
-    from: process.env.EMAIL_USERNAME,
-    to: process.env.EMAIL_USERNAME,
-    subject: 'Testing EmailService',
-    text: 'This email is the result of testing (emailService.spec.js)',
-  };
-
   it('Ctor throws if not provided with a configuration', (done) => {
     assert.throws(() => {
       new EmailService();
@@ -24,7 +17,14 @@ describe('EmailHandler', () => {
     done();
   });
 
-  it('sendEmail sends email', (done) => {
+  it('sendEmail sends one email', (done) => {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+
     const emailService = new EmailService({
       ...emailConfig,
       tls: {
@@ -48,14 +48,88 @@ describe('EmailHandler', () => {
       });
   });
 
-  it('sendMail sends email', (done) => {
-    EmailService
-      .send({message})
+  it('send sends one email', (done) => {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+    EmailService.send({ message })
       .then((result) => {
         const { accepted, rejected, response } = result;
         assert(rejected.length === 0);
         assert(accepted.length === 1);
         assert(accepted[0] === message.to);
+        assert(response.includes('250 2.0.0 Ok: queued as'));
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('send sends multiple emails', (done) => {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: `${process.env.EMAIL_USERNAME},${process.env.EMAIL_USERNAME2}`,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+    EmailService.send({ message })
+      .then((result) => {
+        const { accepted, rejected, response } = result;
+        assert(rejected.length === 0);
+        assert(accepted.length === 2);
+        const recipients = message.to.split(',');
+        assert(accepted[0] === recipients[0]);
+        assert(accepted[1] === recipients[1]);
+        assert(response.includes('250 2.0.0 Ok: queued as'));
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('send sends multiple emails through cc', (done) => {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME,
+      cc: process.env.EMAIL_USERNAME2,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+    EmailService.send({ message })
+      .then((result) => {
+        const { accepted, rejected, response } = result;
+        assert(rejected.length === 0);
+        assert(accepted.length === 2);
+        assert(accepted[0] === message.to);
+        assert(accepted[1] === message.cc);
+        assert(response.includes('250 2.0.0 Ok: queued as'));
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('send sends multiple emails through bcc', (done) => {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME,
+      bcc: process.env.EMAIL_USERNAME2,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+    EmailService.send({ message })
+      .then((result) => {
+        const { accepted, rejected, response } = result;
+        assert(rejected.length === 0);
+        assert(accepted.length === 2);
+        assert(accepted[0] === message.to);
+        assert(accepted[1] === message.bcc);
         assert(response.includes('250 2.0.0 Ok: queued as'));
         done();
       })
