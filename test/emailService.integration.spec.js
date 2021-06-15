c87a6e673e4350e80fbdce922751e28671de76d6
1onlinesolution <team@damianos.io>
2021-06-15 18:34:55 +0100

    improved unit testing

---
+++ test/emailService.integration.spec.js
@@ -0,0 +1,142 @@
+/*
+const assert = require('assert');
+const EmailService = require('../lib/emailService');
+const emailConfig = require('../lib/defaultEmailConfig')({});
+
+describe('EmailHandler', () => {
+  it('Ctor throws if not provided with a configuration', function (done) {
+    assert.throws(() => {
+      new EmailService();
+    }, /configuration cannot be undefined or null/);
+    // or this (see passwordService.spec.js for async/await example):
+    try {
+      new EmailService();
+    } catch (err) {
+      assert(err.name === 'SmtpConfigurationError');
+      assert(err.message === 'SMTP configuration cannot be undefined or null');
+    }
+    done();
+  });
+
+  it('sendEmail sends one email', function (done) {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+
+    const emailService = new EmailService({
+      ...emailConfig,
+      tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false,
+      },
+    });
+
+    emailService
+      .sendEmail(message)
+      .then((result) => {
+        const { accepted, rejected, response } = result;
+        assert(rejected.length === 0);
+        assert(accepted.length === 1);
+        assert(accepted[0] === message.to);
+        assert(response.includes('250 2.0.0 Ok: queued as'));
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('send sends one email', function (done) {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+    EmailService.send({ message })
+      .then((result) => {
+        const { accepted, rejected, response } = result;
+        assert(rejected.length === 0);
+        assert(accepted.length === 1);
+        assert(accepted[0] === message.to);
+        assert(response.includes('250 2.0.0 Ok: queued as'));
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('send sends multiple emails', function (done) {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: `${process.env.EMAIL_USERNAME},${process.env.EMAIL_USERNAME2}`,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+    EmailService.send({ message })
+      .then((result) => {
+        const { accepted, rejected, response } = result;
+        assert(rejected.length === 0);
+        assert(accepted.length === 2);
+        const recipients = message.to.split(',');
+        assert(accepted[0] === recipients[0]);
+        assert(accepted[1] === recipients[1]);
+        assert(response.includes('250 2.0.0 Ok: queued as'));
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('send sends multiple emails through cc', function (done) {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME,
+      cc: process.env.EMAIL_USERNAME2,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+    EmailService.send({ message })
+      .then((result) => {
+        const { accepted, rejected, response } = result;
+        assert(rejected.length === 0);
+        assert(accepted.length === 2);
+        assert(accepted[0] === message.to);
+        assert(accepted[1] === message.cc);
+        assert(response.includes('250 2.0.0 Ok: queued as'));
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('send sends multiple emails through bcc', function (done) {
+    const message = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME,
+      bcc: process.env.EMAIL_USERNAME2,
+      subject: 'Testing EmailService',
+      text: 'This email is the result of testing (emailService.spec.js)',
+    };
+    EmailService.send({ message })
+      .then((result) => {
+        const { accepted, rejected, response } = result;
+        assert(rejected.length === 0);
+        assert(accepted.length === 2);
+        assert(accepted[0] === message.to);
+        assert(accepted[1] === message.bcc);
+        assert(response.includes('250 2.0.0 Ok: queued as'));
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
+*/
--- test/emailService.spec.js
+++ test/emailService.spec.js
@@ -1,9 +1,10 @@
 const assert = require('assert');
+const sinon = require('sinon');
 const EmailService = require('../lib/emailService');
 const emailConfig = require('../lib/defaultEmailConfig')({});
 
-describe('EmailHandler', () => {
-  it('Ctor throws if not provided with a configuration', (done) => {
+describe('Ctor', function () {
+  it('throws if not provided with a configuration', function (done) {
     assert.throws(() => {
       new EmailService();
     }, /configuration cannot be undefined or null/);
@@ -16,15 +17,28 @@ describe('EmailHandler', () => {
     }
     done();
   });
+});
 
-  it('sendEmail sends one email', (done) => {
+describe('EmailHandler', function () {
+  let sandbox = null;
+
+  beforeEach(function () {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('EmailService.sendEmail sends one email', async function () {
     const message = {
-      from: process.env.EMAIL_USERNAME,
-      to: process.env.EMAIL_USERNAME,
+      from: 'fromEmail',
+      to: 'toEmail',
       subject: 'Testing EmailService',
       text: 'This email is the result of testing (emailService.spec.js)',
     };
 
+    const sendEmail = sandbox.stub(EmailService.prototype, 'sendEmail').resolves(true);
     const emailService = new EmailService({
       ...emailConfig,
       tls: {
@@ -32,109 +46,66 @@ describe('EmailHandler', () => {
         rejectUnauthorized: false,
       },
     });
-
-    emailService
-      .sendEmail(message)
-      .then((result) => {
-        const { accepted, rejected, response } = result;
-        assert(rejected.length === 0);
-        assert(accepted.length === 1);
-        assert(accepted[0] === message.to);
-        assert(response.includes('250 2.0.0 Ok: queued as'));
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
+    const result = await emailService.sendEmail(message);
+    assert(result);
+    sinon.assert.calledOnce(sendEmail);
   });
 
-  it('send sends one email', (done) => {
+  it('EmailService.send sends one email', async function () {
     const message = {
-      from: process.env.EMAIL_USERNAME,
-      to: process.env.EMAIL_USERNAME,
+      from: 'fromEmail',
+      to: 'toEmail',
       subject: 'Testing EmailService',
       text: 'This email is the result of testing (emailService.spec.js)',
     };
-    EmailService.send({ message })
-      .then((result) => {
-        const { accepted, rejected, response } = result;
-        assert(rejected.length === 0);
-        assert(accepted.length === 1);
-        assert(accepted[0] === message.to);
-        assert(response.includes('250 2.0.0 Ok: queued as'));
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
+
+    const send = sandbox.stub(EmailService, 'send').resolves(true);
+    const result = await EmailService.send({ message });
+    assert(result);
+    sinon.assert.calledOnce(send);
   });
 
-  it('send sends multiple emails', (done) => {
+  it('EmailService.send sends multiple emails', async function () {
     const message = {
-      from: process.env.EMAIL_USERNAME,
-      to: `${process.env.EMAIL_USERNAME},${process.env.EMAIL_USERNAME2}`,
+      from: 'fromEmail',
+      to: 'toEmail,andAnotherOne',
       subject: 'Testing EmailService',
       text: 'This email is the result of testing (emailService.spec.js)',
     };
-    EmailService.send({ message })
-      .then((result) => {
-        const { accepted, rejected, response } = result;
-        assert(rejected.length === 0);
-        assert(accepted.length === 2);
-        const recipients = message.to.split(',');
-        assert(accepted[0] === recipients[0]);
-        assert(accepted[1] === recipients[1]);
-        assert(response.includes('250 2.0.0 Ok: queued as'));
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
+
+    const sendEmail = sandbox.stub(EmailService, 'send').resolves(true);
+    const result = await EmailService.send({ message });
+    assert(result);
+    sinon.assert.calledOnce(sendEmail);
   });
 
-  it('send sends multiple emails through cc', (done) => {
+  it('EmailService.send sends multiple emails through cc', async function () {
     const message = {
-      from: process.env.EMAIL_USERNAME,
-      to: process.env.EMAIL_USERNAME,
-      cc: process.env.EMAIL_USERNAME2,
+      from: 'fromEmail',
+      to: 'toEmail',
+      cc: 'andAnotherOne',
       subject: 'Testing EmailService',
       text: 'This email is the result of testing (emailService.spec.js)',
     };
-    EmailService.send({ message })
-      .then((result) => {
-        const { accepted, rejected, response } = result;
-        assert(rejected.length === 0);
-        assert(accepted.length === 2);
-        assert(accepted[0] === message.to);
-        assert(accepted[1] === message.cc);
-        assert(response.includes('250 2.0.0 Ok: queued as'));
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
+
+    const sendEmail = sandbox.stub(EmailService, 'send').resolves(true);
+    const result = await EmailService.send({ message });
+    assert(result);
+    sinon.assert.calledOnce(sendEmail);
   });
 
-  it('send sends multiple emails through bcc', (done) => {
+  it('EmailService.send sends multiple emails through bcc', async function () {
     const message = {
-      from: process.env.EMAIL_USERNAME,
-      to: process.env.EMAIL_USERNAME,
-      bcc: process.env.EMAIL_USERNAME2,
+      from: 'fromEmail',
+      to: 'toEmail',
+      bcc: 'andAnotherOne',
       subject: 'Testing EmailService',
       text: 'This email is the result of testing (emailService.spec.js)',
     };
-    EmailService.send({ message })
-      .then((result) => {
-        const { accepted, rejected, response } = result;
-        assert(rejected.length === 0);
-        assert(accepted.length === 2);
-        assert(accepted[0] === message.to);
-        assert(accepted[1] === message.bcc);
-        assert(response.includes('250 2.0.0 Ok: queued as'));
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
+
+    const sendEmail = sandbox.stub(EmailService, 'send').resolves(true);
+    const result = await EmailService.send({ message });
+    assert(result);
+    sinon.assert.calledOnce(sendEmail);
   });
 });
